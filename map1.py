0b460b03c1ba15fda5d850d08ca61019a9389c82
Sremanachittybabu <sremana.chittybabu@gmail.com>
2023-12-19 22:51:37 +0000

    mapchanges

--- map1.py
+++ map1.py
@@ -4,3 +4,4 @@ fg=folium.FeatureGroup(name="MyMap")
 fg.add_child(folium.Marker(location=[38.2, -99.01], popup="Hi Iam a marker", icon=folium.Icon(color='green')))
 map.add_child(fg)
 map.save("Map2.html")
+
