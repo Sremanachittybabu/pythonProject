1fee74cba5f0a501e47588915dd5f7fb5f065370
Sremanachittybabu <sremana.chittybabu@gmail.com>
2023-12-20 15:36:05 +0000

    test

--- map2.py
+++ map2.py
@@ -4,4 +4,4 @@ fg=folium.FeatureGroup(name="MyMap")
 for coordinates in [[38.2, -99.01],[48.2, -89.01]]:
     fg.add_child(folium.Marker(location=coordinates, popup="Hi Iam a marker", icon=folium.Icon(color='green')))
 map.add_child(fg)
-map.save("Map1.html")
+map.save("Map2.html")
