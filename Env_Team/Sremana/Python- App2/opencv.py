c451877c4384e2138f205e77bae8c44391a5ea7d
Sremanachittybabu <sremana.chittybabu@gmail.com>
2024-01-03 20:43:41 +0000

    mapchanges

--- Env_Team/Sremana/Python- App2/opencv.py
+++ Env_Team/Sremana/Python- App2/opencv.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread("galaxy.jpg",0)
+img = cv2.imread("galaxy.jpg", 0)
 
 print(type(img))
 print(img)
@@ -9,7 +9,7 @@ print(img.ndim)
 
 resized_image = cv2.resize(img,(int(img.shape[1]/4),int(img.shape[0]/4)))
 cv2.imshow("Galaxy",img)
-cv2.imwrite("Galaxy_resized.jpg",resized_image)
+cv2.imwrite("Galaxy_resized.jpg", resized_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
